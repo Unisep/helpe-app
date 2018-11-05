f4bc157a1bac86e44c89ea826b7eecb8483321b8
allan_horst <allanhorstwillig@gmail.com>
2018-11-04 22:38:55 -0200

    Link para ask question

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import 'home.css'
 
 export default class Home extends React.Component {
   render() {
     return (
-      <h1>Home</h1>
+      <div className="home">
+        <div className="button-ask">
+          <h3>Recently Questions</h3>
+          <a className="button primary" href="/question/ask">Ask question</a>
+        </div>
+      </div>
     )
   }
 }
